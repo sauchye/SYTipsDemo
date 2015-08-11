523967c47f4e89653a7f698d3af5fc036b3c9f52
Sauchye <sauchyer@gmail.com>
2015-08-11 23:42:54 +0800

    Initial Commit

---
+++ SYTipsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SYTipsDemo
+//
+//  Created by Sauchye on 8/11/15.
+//  Copyright (c) 2015 com.sauchye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SYTipsDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SYTipsDemo
+//
+//  Created by Sauchye on 8/11/15.
+//  Copyright (c) 2015 com.sauchye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
